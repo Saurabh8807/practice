ff35f4287d9b829dec88ce29829ac67471ed5b74
saurabhJagtap8807 <saurabhjagtap@golokait.com>
2024-05-01 22:21:48 +0530

    Your commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <h1 className=' m-5 text-3xl font-bold text-blue-800'>Hello World</h1>
+      <h1 className=' m-3 text-3xl font-bold text-blue-800'>Hello World</h1>
     </>
   )
 }
